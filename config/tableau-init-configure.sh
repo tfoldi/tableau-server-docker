7e39a02a2d1c2ba7f1ce60bf27e36423acd0d7d9
dkuida <dan@kuida.org>
2018-09-23 11:46:45 +0200

    spellign correction and removal of --restart flag that is invalid in current version

--- config/tableau-init-configure.sh
+++ config/tableau-init-configure.sh
@@ -6,13 +6,13 @@ set -e
 LOGFILE=/var/log/tableau_docker.log
 RETAIN_NUM_LINES=10000
 
-function logsetup {  
+function logsetup {
     TMP=$(tail -n $RETAIN_NUM_LINES $LOGFILE 2>/dev/null) && echo "${TMP}" > $LOGFILE
     exec > >(tee -a $LOGFILE)
     exec 2>&1
 }
 
-function log {  
+function log {
     echo "[$(date --rfc-3339=seconds)]: $*"
 }
 
@@ -24,9 +24,9 @@ if [ -f /opt/tableau/docker_build/.init-done ]; then
 
 else
 
-    log start initalize tsm
+    log start initialize tsm
     su tsm -c "sudo sh -x /opt/tableau/tableau_server/packages/scripts.*/initialize-tsm -f --accepteula" 2>&1 1> /var/log/tableau_docker.log
-    log initalize done
+    log initialize done
 
     source /etc/profile.d/tableau_server.sh
 
@@ -36,9 +36,9 @@ else
 
     log licenses activate
     su tsm -c "sudo /opt/tableau/tableau_server/packages/customer-bin.${TABLEAU_SERVER_DATA_DIR_VERSION}/tsm licenses activate -t" 2>&1 1>> /var/log/tableau_docker.log
-    log licenses activate done 
+    log licenses activate done
 
-    log register 
+    log register
     su tsm -c "sudo /opt/tableau/tableau_server/packages/customer-bin.${TABLEAU_SERVER_DATA_DIR_VERSION}/tsm register --file /opt/tableau/docker_build/registration_file.json" 2>&1 1>> /var/log/tableau_docker.log
     log register done
 
@@ -47,14 +47,14 @@ else
     log settings import done
 
     log pending-changes apply
-    su tsm -c "sudo /opt/tableau/tableau_server/packages/customer-bin.${TABLEAU_SERVER_DATA_DIR_VERSION}/tsm pending-changes apply --restart" 2>&1 1>> /var/log/tableau_docker.log
-    log penging-changes apply done
+    su tsm -c "sudo /opt/tableau/tableau_server/packages/customer-bin.${TABLEAU_SERVER_DATA_DIR_VERSION}/tsm pending-changes apply" 2>&1 1>> /var/log/tableau_docker.log
+    log pending-changes apply done
 
-    log initalize server
+    log initialize server
     su tsm -c "sudo /opt/tableau/tableau_server/packages/customer-bin.${TABLEAU_SERVER_DATA_DIR_VERSION}/tsm initialize --start-server --request-timeout 1800" 2>&1 1>> /var/log/tableau_docker.log
-    log initalize server done
+    log initialize server done
 
-    log initialuser 
+    log initialuser
     su tsm -c "sudo /opt/tableau/tableau_server/packages/bin.${TABLEAU_SERVER_DATA_DIR_VERSION}/tabcmd initialuser --server localhost:80 --username admin --password admin" 2>&1 1>> /var/log/tableau_docker.log
     log all done
 
